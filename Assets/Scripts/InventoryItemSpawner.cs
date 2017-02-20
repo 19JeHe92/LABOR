a48b9e80114bac89e2b451fda1895a9678364aa0
iszen <szenbella@gmail.com>
2017-02-20 10:34:25 +0100

    added SMAA and extended the inventory. You're now able to remove items

---
+++ Assets/Scripts/InventoryItemSpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using NewtonVR;
+using System;
+
+public class InventoryItemSpawner : MonoBehaviour
+{
+
+    public GameObject prefabToSpawn;
+    public InventoryObjectType typeToRemove;
+    public NVRHand leftHand;
+    public NVRHand rightHand;
+    public Vector3 offsetToActivateSpawner = new Vector3(0.1f, 0.1f, 0.1f);
+    public InventoryController inevtoryController;
+
+    void Update()
+    {
+        Vector3 offsetToLeftHand = Vector3Abs(leftHand.transform.position - transform.position);
+        Vector3 offsetToRightHand = Vector3Abs(rightHand.transform.position - transform.position);
+        if ((offsetToActivateSpawner.x > offsetToLeftHand.x && offsetToActivateSpawner.y > offsetToLeftHand.y && offsetToActivateSpawner.z > offsetToLeftHand.z))
+        {
+            Debug.Log("Left Hand colliding with inventory object");
+            SpawnObjectInHand(leftHand);
+        }
+        else if (offsetToActivateSpawner.x > offsetToRightHand.x && offsetToActivateSpawner.y > offsetToRightHand.y && offsetToActivateSpawner.z > offsetToRightHand.z)
+        {
+            Debug.Log("Right Hand colliding with inventory object");
+            SpawnObjectInHand(rightHand);
+        }
+    }
+
+    private void SpawnObjectInHand(NVRHand hand)
+    {
+        if (hand.Inputs[NVRButtons.Grip].PressDown)
+        {
+            //if (inevtoryController.RemovefromInventory(typeToRemove))
+            //{
+                Debug.Log("Successfully removed item");
+                //Spawn an object and make the hand interact with it
+                GameObject spawnedObject = (GameObject)Instantiate(prefabToSpawn, transform.position, transform.rotation);
+                hand.BeginInteraction(spawnedObject.GetComponent<NVRInteractableItem>());
+            //}
+        }
+    }
+
+    private Vector3 Vector3Abs(Vector3 vec3)
+    {
+        Vector3 result = new Vector3(Math.Abs(vec3.x), Math.Abs(vec3.y), Math.Abs(vec3.z));
+        return result;
+    }
+}
