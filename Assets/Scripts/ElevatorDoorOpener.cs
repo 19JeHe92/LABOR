13d172903be90e7610c02ede7f4ab75b9bfbbf31
iszen <szenbella@gmail.com>
2017-03-08 17:53:54 +0100

    added elevator opener

---
+++ Assets/Scripts/ElevatorDoorOpener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ElevatorDoorOpener : MonoBehaviour
+{
+
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public Transform leftEnd;
+    public Transform rightEnd;
+    public float openingSpeed;
+    public bool isOpen = false;
+
+    void Update()
+    {
+        if (isOpen)
+        {
+            float step = openingSpeed * Time.deltaTime;
+            leftDoor.transform.position = Vector3.MoveTowards(leftDoor.transform.position, leftEnd.position, step);
+            rightDoor.transform.position = Vector3.MoveTowards(rightDoor.transform.position, rightEnd.position, step);
+        }
+    }
+}
