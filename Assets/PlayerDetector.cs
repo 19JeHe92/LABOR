d4780cca324694955ccacb9b00bb29345b98aeab
iszen <szenbella@gmail.com>
2017-03-09 17:49:35 +0100

    added puzzle objects

---
+++ Assets/PlayerDetector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using NewtonVR;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayerDetector : MonoBehaviour {
+
+    public NVRHead player;
+    public float limit = 3.0f;
+    public int secondsBeforeLoad = 3;
+
+    private Vector3 offset;
+
+	void Update ()
+    {
+        offset = player.transform.position - transform.position;
+        if (Mathf.Abs(offset.x) < limit || Mathf.Abs(offset.z) < limit)
+        {
+            StartCoroutine(LoadSceneAfterSeconds());
+        }	
+	}
+
+    IEnumerator LoadSceneAfterSeconds()
+    {
+        yield return new WaitForSeconds(secondsBeforeLoad);
+        SceneManager.LoadScene("ChP4-Smoke");
+    }
+}
