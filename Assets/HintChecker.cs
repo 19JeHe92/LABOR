27dfa55f87faae227ed58cb5f7324ddae657d785
iszen <szenbella@gmail.com>
2017-03-10 17:08:14 +0100

    added hint sound

---
+++ Assets/HintChecker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using NewtonVR;
+
+public class HintChecker : MonoBehaviour {
+
+    public NVRInteractableItem hint1;
+    public NVRInteractableItem hint2;
+    public NVRInteractableItem hint3;
+
+    public AudioSource hintSound;
+
+    private bool hintSoundPlayed = false;
+
+    void Update()
+    {
+        if(isPlayerInteractingWithHint() && !hintSoundPlayed)
+        {
+            hintSound.Play();
+            hintSoundPlayed = true;
+        }
+    }
+
+    private bool isPlayerInteractingWithHint()
+    {
+        return hint1.IsAttached || hint2.IsAttached || hint3.IsAttached;
+    }
+}
