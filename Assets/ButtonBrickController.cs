9bbdfdaddc66c1490417e2a9ea11ac96ba99c3a2
iszen <szenbella@gmail.com>
2017-03-14 05:16:43 +0100

    added attach joint and point to SHP5

---
+++ Assets/ButtonBrickController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using NewtonVR;
+
+public class ButtonBrickController : MonoBehaviour {
+
+    public NVRButton button;
+    public AudioSource comeOutSound;
+    public float speed;
+    public Transform endPos;
+
+    private bool comeOut;
+
+	// Update is called once per frame
+	void Update () {
+        if (!comeOut && button.ButtonIsPushed)
+        {
+            comeOut = true;
+            comeOutSound.Play();
+        }
+        if (comeOut)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos.position, speed*Time.deltaTime);
+        }
+	}
+}
