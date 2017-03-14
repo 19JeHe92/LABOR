5acdbdc4aa5f8a7306413837b9b9565744ae65c6
iszen <szenbella@gmail.com>
2017-03-14 10:17:09 +0100

    added Zombie releaser to ZombieCapsuleController

--- Assets/Scripts/ZombieCapsuleController.cs
+++ Assets/Scripts/ZombieCapsuleController.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
+using NewtonVR;
 
 public class ZombieCapsuleController : MonoBehaviour
 {
-
+    public NVRHead player;
     public float secondsBeforeRelease = 15f;
-    public GameObject Zombie;
+    public GameObject floatingZombie;
+    public GameObject attackingZombiePrefab;
+    public GameObject CapsuleGlas;
     public bool isReleased = false;
+    public Transform disablingEndPosition;
+    public float disblingspeed = 2;
 
+    public audioSource releaseSound;
+
+    private bool disabling;
     private float counter = 0.0f;
 
     void Update()
     {
-        if (counter > secondsBeforeRelease)
+        if (!isReleased && !disabling && counter > secondsBeforeRelease)
         {
             ReleaseZombie();
         }
@@ -19,17 +27,25 @@ public class ZombieCapsuleController : MonoBehaviour
         {
             counter += Time.deltaTime;
         }
+        if (disabling)
+        {
+            floatingZombie.transform.position = Vector3.MoveTowards(transform.position, disablingEndPosition.position, disblingspeed*Time.delstaTime);
+        }
     }
 
     private void ReleaseZombie()
     {
+        releaseSound.Play();
         isReleased = true;
-        //TODO
+        Destroy(CapsuleGlas);
+        GameObject releasedZombie = Instatiate(attackingZombiePrefab, floatingZombie.transform);
+        Zombie zombie = releasedZombie.GetComponent<Zombie>();
+        zombie.attack(player);
+        Destroy(floatingZombie);
     }
 
     public void DisableZombie()
     {
-        //TODO
-        //Destroy(gameObject);
+        disabling = true;
     }
 }
