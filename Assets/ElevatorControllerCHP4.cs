39f708bee2221c61a93a8bca3f61eed48ace2dfb
iszen <szenbella@gmail.com>
2017-03-14 12:03:04 +0100

    added elevator controller to CHP4

--- Assets/ElevatorControllerCHP4.cs
+++ Assets/ElevatorControllerCHP4.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ElevatorControllerCHP4 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+    public float SecondsBeforeStart = 1;
+    public float secondsBeforeBreakDown = 5;
+    public float secondsBeforeHint = 10;
+    public float secondsBetweenCoughs = 4;
+
+    public AudioSource elevatorSound;
+    public AudioSource BreakDownSound;
+    public AudioSource hintSound;
+    public AudioSource coughSound;
+
+    public GameObject smokePrefab;
+
+    private bool isStarted = false;
+    private bool isBroken = false;
+    private bool wasHintPlayed = false;
+    private bool cough = false;
+
+    private float timer = 0;
+    private float coughTimer = 0;
+
 	void Update () {
-		
+        timer += Time.deltaTime;
+        if(!isStarted && timer > SecondsBeforeStart)
+        {
+            isStarted = true;
+            elevatorSound.Play();
+        }
+        if (!isBroken && timer > secondsBeforeBreakDown)
+        {
+            isBroken = true;
+            BreakDownSound.Play();
+            Instantiate(smokePrefab);
+            cough = true;
+        }
+        if(!wasHintPlayed && timer > secondsBeforeHint)
+        {
+            wasHintPlayed = true;
+            hintSound.Play();
+        }
+        if (cough)
+        {
+            coughTimer += Time.deltaTime;
+            if (coughTimer > secondsBetweenCoughs)
+            {
+                coughSound.Play();
+                coughTimer = 0;
+            }
+        }
 	}
 }
