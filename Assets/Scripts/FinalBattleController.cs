5f7ab6421890545fc8a522d4d3970e471ed03234
iszen <szenbella@gmail.com>
2017-03-15 14:59:37 +0100

    added scripts for final battle

---
+++ Assets/Scripts/FinalBattleController.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using NewtonVR;
+using UnityEngine.SceneManagement;
+
+public class FinalBattleController : MonoBehaviour {
+
+    public Transform player;
+    public HealthBarController playerHealth;
+    
+    public FinalBoss boss;
+    public GameObject zombiePrefab;
+    public float secondsBeforeStart = 3f;
+    public float talkDuration = 5f;
+    public float secondsBeforeFirstRoundOfZombies = 20f;
+    public float secondsBossFirstAttack = 3;
+
+    public Transform ZombieSpawningPosition1;
+    public Transform ZombieSpawningPosition2;
+    public Transform ZombieSpawningPosition3;
+    public Transform ZombieSpawningPosition4;
+
+    private float timer = 0;
+    private bool isStarted = false;
+    private bool firstRoundStarted = false;
+    private bool secondRoundStarted = false;
+    private bool firstRoundComplete = false;
+
+    private Zombie zombie1;
+    private Zombie zombie2;
+    private Zombie zombie3;
+    private Zombie zombie4;
+    private Zombie zombie5;
+    private Zombie zombie6;
+
+    private bool complete = false;
+
+    void Awake()
+    {
+        boss.playerHealth = playerHealth;
+    }
+
+    void Update ()
+    {
+
+        if (boss == null && !complete)
+        {
+            complete = true;
+            Debug.Log("GameComplete");
+            SceneManager.LoadScene("StudentRoom");
+        }
+        timer += Time.deltaTime;
+        if(!isStarted && timer > secondsBeforeStart)
+        {
+            isStarted = true;
+            StartBattle();
+            timer = 0f;
+        }
+        if (timer > secondsBeforeFirstRoundOfZombies)
+        {
+            boss.Shout();
+            SpawnFirstRoundZombies();
+        }
+        if (firstRoundStarted && zombie1 == null && zombie2 == null && !firstRoundComplete)
+        {
+            firstRoundComplete = true;
+            
+            boss.AttackPlayerAndGoBackAfterSeconds(player, secondsBossFirstAttack);
+        }
+        if (secondRoundStarted && zombie2 == null && zombie3 == null && !secondRoundComplete)
+        {
+            secondRoundComplete = true;
+            firstRoundComplete = true;
+            boss.pl
+            boss.AttackPlayerUntilDead(player);
+        }
+    }
+
+    private void SpawnFirstRoundZombies()
+    {
+        GameObject zombie1 = Instantiate(zombiePrefab, ZombieSpawningPosition1);
+        GameObject zombie2 = Instantiate(zombiePrefab, ZombieSpawningPosition2);
+        //TODO make zombies attack hte player
+        firstRoundStarted = true;
+    }
+
+    private void SpawnSecondRoundZombies()
+    {
+        GameObject zombie3 = Instantiate(zombiePrefab, ZombieSpawningPosition1);
+        GameObject zombie4 = Instantiate(zombiePrefab, ZombieSpawningPosition2);
+        GameObject zombie5 = Instantiate(zombiePrefab, ZombieSpawningPosition3);
+        GameObject zombie6 = Instantiate(zombiePrefab, ZombieSpawningPosition4);
+        //TODO make zombies attack hte player
+        secondRoundStarted = true;
+    }
+
+    private void StartBattle()
+    {
+        boss.Talk();
+        boss.TurnAroundAfter(talkDuration);
+    }
+}
---
+++ Assets/Scripts/FinalBoss.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FinalBoss : MonoBehaviour//, Enemy
+{
+    private Animator animator;
+    public AudioSource talk;
+    public AudioSource shoutSound;
+    public AudioSource getHitSound;
+    public AudioSource dieSound;
+
+    public float damage = 10f;
+    public float SecondsBetweenHits = 2;
+    public float hitLimit = 1f;
+    private bool isHitting = false;
+
+    public float walkSpeed = 2;
+    public float turnSpeed = 0.2f;
+
+    public bool isImmune = true;
+
+    private Transform Target;
+    public HealthBarController playerHealth;
+    private bool move;
+    private bool turn;
+    private float degreesTurned;
+    private EnemyHealth health;
+    private Transform initilalPosition;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        health = GetComponent<EnemyHealth>();
+        initilalPosition = transform;
+    }
+
+    void Update()
+    {
+
+        if(!isHitting && Vector3.Distance(Target.position, transform.position) < hitLimit)
+        {
+            Hit();
+        }else if (isHitting && Vector3.Distance(Target.position, transform.position) >= hitLimit)
+        {
+            isHitting = false;
+            WalkTo(Target);
+        }
+
+        if (isImmune && health.enabled)
+        {
+            health.enabled = false;
+        }
+        else if (!isImmune && !health.enabled)
+        {
+            health.enabled = true;
+        }
+        if (move)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Target.position, walkSpeed * Time.deltaTime);
+        }
+        if (turn)
+        {
+            Vector3 to = new Vector3(0, 180, 0);
+            if (Vector3.Distance(transform.eulerAngles, to) > 0.01f)
+            {
+                transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, to, Time.deltaTime);
+            }
+            else
+            {
+                transform.eulerAngles = to;
+                turn = false;
+            }
+        }
+    }
+
+    public void TurnAroundAfter(float seconds)
+    {
+        StartCoroutine(WaitAndTurn(seconds));
+    }
+
+    public void Idle()
+    {
+        animator.SetTrigger("idle");
+    }
+
+    public void Shout()
+    {
+        animator.SetTrigger("shout");
+        shoutSound.Play();
+    }
+
+    public void Hit()
+    {
+        animator.SetTrigger("attack");
+        isHitting = true;
+    }
+
+    public void WalkTo(Transform to)
+    {
+        Target = to;
+        animator.SetTrigger("walk");
+    }
+
+    public void GetHit()
+    {
+        if (!isImmune)
+        {
+            animator.SetTrigger("gethit");
+            getHitSound.Play();
+        }
+    }
+
+    public void Die()
+    {
+        animator.SetTrigger("die");
+        dieSound.Play();
+        Destroy(gameObject, 3);
+    }
+
+    public void Talk()
+    {
+        talk.Play();
+    }
+
+    private IEnumerator WaitAndTurn(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        turn = true;
+    }
+
+    public void AttackPlayerAndGoBackAfterSeconds(Transform player, float seconds)
+    {
+        WalkTo(player);
+        StartCoroutine(SetTargetAfterSeconds(initilalPosition, seconds));
+    }
+
+    private IEnumerator SetTargetAfterSeconds(Transform target,  float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        this.Target = target;
+    }
+
+}
