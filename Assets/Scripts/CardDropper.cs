2d26f5f56c46a788f681b0af4215902f6cf35704
iszen <szenbella@gmail.com>
2017-03-16 05:02:13 +0100

    added card dropper

---
+++ Assets/Scripts/CardDropper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CardDropper : MonoBehaviour {
+
+    public GameObject robot;
+    public GameObject card;
+
+    public AudioSource hintToGoInOtherDirection;
+	
+	// Update is called once per frame
+	void Update () {
+        if (robot == null && !card.active)
+        {
+            card.active = true;
+        }
+	}
+
+    private IEnumerator WaitAndPlaySound()
+    {
+        yield return new WaitForSeconds(2);
+        hintToGoInOtherDirection.Play();
+    }
+}
